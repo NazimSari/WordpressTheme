28867dafae42fe9d75e9d205729e7321f487cf0f
Nazim <yasarnazimsari@gmail.com>
2023-12-24 23:51:58 +0300

    style ve script dosya  rotaları düzenlendi

--- header.php
+++ header.php
@@ -9,11 +9,13 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attributes(); ?>">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
     <title>Evolution Theme</title>
 </head>
 <body>
+    <?php wp_body_open(); ?>
     <header>Header</header>
