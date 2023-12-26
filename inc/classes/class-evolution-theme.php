f1058c0ad1445eed206843ca5856ff3f75de29f8
Nazim <yasarnazimsari@gmail.com>
2023-12-26 13:45:36 +0300

    dynamic title tag and logo

--- inc/classes/class-evolution-theme.php
+++ inc/classes/class-evolution-theme.php
@@ -14,11 +14,28 @@ class EVOLUTION_THEME {
 
     protected function __construct(){
         //load class.
-        $this-> set_hooks();
+
+        $this-> setup_hooks();
     }
 
-    protected function set_hooks(){
+    protected function setup_hooks(){
         //action and filters
+
+        add_action('after_setup_theme', [$this, 'setup_theme']);
+
+    }
+
+    public function setup_theme(){
+        add_theme_support('title-tag');
+
+        add_theme_support('custom-logo', [
+        'height'               => 100,
+		'width'                => 400,
+		'flex-height'          => true,
+		'flex-width'           => true,
+		'header-text'          => ['site-title', 'site-description' ],
+		'unlink-homepage-logo' => true, 
+        ]);
     }
 }
 
